2395a1b35f189f93590e3808ca44e718b7e40cc2
Piotr Marendowski <piotr-marendowski@tutanota.com>
2023-09-15 19:20:13 +0000

    Update site

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function question(answer)
         document.getElementById("answer").scrollIntoView();
     }
 }
+const fs = require('fs');
+const zeroMd = require('zero-md');
+
+const folderPath = './posts/';
+const zeroMdTag = document.querySelector('zero-md');
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  files.forEach((file) => {
+    if (file.endsWith('.md')) {
+      const filePath = folderPath + file;
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const html = zeroMd(data);
+        zeroMdTag.innerHTML += html;
+      });
+    }
+  });
+});
